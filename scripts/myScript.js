d10113c674ce42183843a2eccfedc0ff5aa2a298
Daniel McGill <danm364@yahoo.com>
2023-05-07 13:35:40 -0400

    add settimeout feature correctly

--- scripts/myScript.js
+++ scripts/myScript.js
@@ -1,39 +1,49 @@
-const defaultCommentsData = [
-    {
-        name: "Connor Walton",
-        date: "02/17/2021",
-        paragraph: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
+// const defaultCommentsData = [
+//     {
+//         name: "Connor Walton",
+//         date: "02/17/2021",
+//         paragraph: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
 
-    },
-    {
-        name: "Emilie Beach",
-        date: "01/09/2021",
-        paragraph: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day."
+//     },
+//     {
+//         name: "Emilie Beach",
+//         date: "01/09/2021",
+//         paragraph: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day."
 
-    },
-    {
-        name: "Miles Acosta",
-        date: "12/20/2020",
-        paragraph: "I can t stop listening. Every time I hear one of their songs the vocals it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can t get enough."
-    }
-]
+//     },
+//     {
+//         name: "Miles Acosta",
+//         date: "12/20/2020",
+//         paragraph: "I can t stop listening. Every time I hear one of their songs the vocals it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can t get enough."
+//     }
+// ]
+
+
 
 
 
 
 function createComments() {
+    let showsRequest = axios.get(request_comments)
+    showsRequest.then((resolve)=> {
+       console.log(resolve)
+    
     //Parent container
     const parentNode = document.querySelector('.feed')
     const parentContainer = document.createElement('div')
     parentContainer.classList.add('feed__parent')
-    
+
+    resolve.data.sort((a, b) => {
+        return b.timestamp - a.timestamp
+    })
+
     //create/get all the nodes that we will need
 
-    for (let i = 0; i < defaultCommentsData.length; i++) {
+    for (let i = 0; i < resolve.data.length; i++) {
 
         //add everything to this one node than add all comments to this node add to dom than if comment added remove all than add again
         
-
+    
 
         const containerNode = document.createElement('div')
         const headerNode = document.createElement('div')
@@ -51,9 +61,12 @@ function createComments() {
         dateNode.classList.add("feed__date")
         imgNode.classList.add("feed__user-img")
 
-        nameNode.innerText = defaultCommentsData[i].name
-        dateNode.innerText = defaultCommentsData[i].date
-        textNode.innerText = defaultCommentsData[i].paragraph
+
+        const commentDate = new Date(resolve.data[i].timestamp).toLocaleDateString()
+
+        nameNode.innerText = resolve.data[i].name
+        dateNode.innerText = commentDate
+        textNode.innerText = resolve.data[i].comment
         imgNode.src = "./assets/Logo/grey.png"
 
         parentContainer.appendChild(containerNode)
@@ -62,12 +75,13 @@ function createComments() {
     }
 
     parentNode.appendChild(parentContainer)
-    
+})
+
 }
+    
+    
 
-createComments()
-
-function addComment() {
+function addComment(request_comments, api_key) {
     const parentContainer = document.querySelector('.feed__parent')
     //need to remove all the comments to rerender new list with new comment
     parentContainer.remove()
@@ -107,7 +121,6 @@ function addComment() {
 
     //change border if there is error if not create comment
     if (nameNode.innerText === '' && textNode.innerText === '') {
-        console.log(textNode.style)
         nameField.style.border = "1px solid red";
         textField.style.border = "1px solid red";
         
@@ -124,31 +137,52 @@ function addComment() {
     
     //add comment to object array
     else {
-        newComment = {
-            name: nameNode.innerText,
-            date: dateNode.innerText,
-            paragraph: textNode.innerText = commentInput
-        }
 
-        defaultCommentsData.unshift(newComment)
+        base_url = `https://project-1-api.herokuapp.com/comments/?api_key=${api_key}`
+
+
+        const postItem = axios.post(base_url, {
+            
+            name: `${nameNode.innerText}`,
+            comment: `${textNode.innerText}`,
+            
+            
+            
+        
+
+        }).then((resolve) => {
+            console.log(resolve)
+        })
     }
 
 }
 
 
+const api_key = "c9d949b8-3905-4cba-af1d-6cfe9874da94"
+request_comments = `https://project-1-api.herokuapp.com/comments/?api_key=${api_key}`
+
+
+createComments()
 const commentForm = document.querySelector('.comment__form')
 
 //add event listener on submit that go through logic
 commentForm.addEventListener("submit", (e) => {
     
     e.preventDefault()
-    addComment()
-    createComments()
+    addComment(request_comments, api_key)
+    setTimeout(createComments, 500)
+    
+    
+    //need to remove all the comments to rerender new list with new comment
+    
+    
+    
+    
+    
     //after comment is created reset inputs to blank
     e.target[0].value = ''
     e.target[1].value = ''
 });
-
 
 
 
